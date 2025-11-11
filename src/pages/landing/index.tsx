720f80c9c62458817027ea4df4e2d3f8f05b885f
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-11-11 17:46:46 +0330

    feat: landing page

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -2,7 +2,13 @@ import s from "./landing.module.css";
 
 const LandingPage = () => {
     return (
-        <>Yo</>
+        <main className={s.container}>
+            <h1>BISMARCK is offline</h1>
+            <section>
+                <h2>Would you like to wake it?</h2>
+                <button>Wake</button>
+            </section>
+        </main>
     );
 };
 
